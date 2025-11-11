76f377e40cb079b7ea8ffde45ca2314a274ce03b
Zain <m.zainnajeeb@gmail.com>
2025-11-11 17:51:25 -0500

    Removing

--- scraper/main.py
+++ scraper/main.py
@@ -2,15 +2,5 @@ from shared.mongo import get_mongo_client, get_database
 
 
 if __name__ == "__main__":
-    client = get_mongo_client()
-    print("Connected to MongoDB")
-    
-    # Get the postings collection from housing database
-    collection = get_database(client)
-    
-    # Read all documents from the postings collection
-    documents = list(collection.find())
-    print(f"Found {len(documents)} documents in postings collection:")
-    for doc in documents:
-        print(doc)
+    pass
     
